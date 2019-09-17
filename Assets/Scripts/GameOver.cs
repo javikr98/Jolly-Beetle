3e930e431d0587d906481b73a3da9adb950a6d73
Javier Egea <javieglo98@gmail.com>
2019-09-17 13:40:44 +0200

    Initial commit

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameOver : MonoBehaviour
+{
+    public Text highscore;
+
+    void Start()
+    {
+        highscore.text = "Highscore: " + PlayerPrefs.GetInt("highscore");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Text highscore;
+
+    void Start()
+    {
+        highscore.text = "Highscore: " + PlayerPrefs.GetInt("highscore");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Play();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Reset()
+    {
+        PlayerPrefs.SetInt("highscore", 0);
+        highscore.text = "Highscore: " + PlayerPrefs.GetInt("highscore");
+    }
+}
